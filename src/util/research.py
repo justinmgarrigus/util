f39d2016ccf94fe660915a4cedb28f6706b8a50f
justinmgarrigus <justin.m.garrigus@gmail.com>
2026-07-07 22:50:21 -0500

    Catch new error

--- src/util/research.py
+++ src/util/research.py
@@ -491,6 +491,9 @@ class Artifact:
                         )
 
                     # Copy it. Works for files or directories.
+                    if not os.path.exists(obj): 
+                        raise FileNotFoundError(obj) 
+
                     if obj.is_file():
                         shutil.copy(obj, path)
                     elif obj.is_dir():
--- test/test_research.py
+++ test/test_research.py
@@ -782,6 +782,26 @@ class TestArtifact:
                 with open(art.props[f"f{j}"], "r") as f:
                     assert f.read() == f"multiple arts same exp {i} {j}"
 
+    def test_add_artifact_missing_file(self: "TestArtifact") -> None:
+        """
+        Adding an artifact which contains a missing file should give the correct
+        error. 
+        """
+        
+        os.environ["RESEARCH_PATH"] = DATA_DIR
+        exp = Experiment(name="a", ident="foo", description="b")
+
+        art = Artifact(
+            experiment=exp, 
+            ident="art", 
+            props={"f": pathlib.Path("foo.txt")}
+        )
+        try:
+            exp.add_artifact(art)
+            assert False
+        except FileNotFoundError:
+            pass 
+
     def test_copy_directory(self: "TestArtifact") -> None:
         """
         Copies an entire directory instead of just a file.
