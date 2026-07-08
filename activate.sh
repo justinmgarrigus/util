286066579857166d57d3b0e25a1f1e5667325616
justinmgarrigus <justin.m.garrigus@gmail.com>
2026-07-07 20:23:50 -0500

    Added "lint" command

--- activate.sh
+++ activate.sh
@@ -2,9 +2,11 @@
 
 # Source this file in your ".bashrc" to add the scripts to your environment.
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-if [ -z $SCRIPT_DIR/.git ]; then 
+GIT_ROOT=$(cd "$SCRIPT_DIR" && git rev-parse --show-toplevel) 
+if [ -z $GIT_ROOT/.git ]; then 
     echo "Error: no .git found for util project!"
 fi 
-alias archive="uv --directory "$SCRIPT_DIR" run bin/archive" 
-alias unarchive="uv --directory "$SCRIPT_DIR" run bin/unarchive" 
-alias telegram-message="uv --directory "$SCRIPT_DIR" run bin/telegram-message" 
+alias archive="uv --directory "$GIT_ROOT" run bin/archive" 
+alias unarchive="uv --directory "$GIT_ROOT" run bin/unarchive" 
+alias telegram-message="uv --directory "$GIT_ROOT" run bin/telegram-message" 
+alias lint="uv --directory "$GIT_ROOT" run bin/lint"
